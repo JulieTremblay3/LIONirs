437ceb41af96d9c5dc8f21a6bc2ff2f53b525401
lionlabuser <64552192+lionlabuser@users.noreply.github.com>
2021-04-01 17:18:40 -0400

    Ajustement de localisation AUX (nirs adjustment branch)
    possibilite de tenir compte du subfolder dans le fichier AUX

--- nirs_run_NIRSmatdiradjust.m
+++ nirs_run_NIRSmatdiradjust.m
@@ -8,79 +8,89 @@ for imodule = numel(NIRS.Dt.fir.pp):-1:1
         1;
     end
     for ifile = 1:numel(NIRS.Dt.fir.pp(imodule).p)
-    %Find the new root dir 
+        %Find the new root dir
         fileloc_previous = NIRS.Dt.fir.pp(imodule).p{ifile};
         [dir1,file1,ext1]=fileparts(fileloc_previous);
-    %Descendre les répertoire pour trouver la partie qui doit être modifier
-    %
-    [toknew,remnew]=strtok(fliplr(NIRSmatnewdir),{'\','/'});
-    [tok1,rem1]=strtok(fliplr(dir1),{'\','/'});
-    ok = 1;
-    while ok
-     if  strcmp(toknew, tok1)
-         dirrootnew = [fliplr(remnew),fliplr(toknew)];
-        ok=0; %getout ! 
-     else
-         [toknew,remnew]=strtok(remnew,{'\','/'});
-     end
-     if isempty(toknew)
-         ok=0; 
-         disp([fliplr(tok1) ,' root folder do not exist'])
-     end
-         
+        %Descendre les répertoire pour trouver la partie qui doit être modifier
+        %
+        [toknew,remnew]=strtok(fliplr(NIRSmatnewdir),{'\','/'});
+        [tok1,rem1]=strtok(fliplr(dir1),{'\','/'});
+        ok = 1;
+        while ok
+            if  strcmp(toknew, tok1)
+                dirrootnew = [fliplr(remnew),fliplr(toknew)];
+                ok=0; %getout !
+            else
+                [toknew,remnew]=strtok(remnew,{'\','/'});
+            end
+            if isempty(toknew)
+                ok=0;
+                disp([fliplr(tok1) ,' root folder do not exist'])
+            end
+            
+        end
+        NIRS.Dt.fir.pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
     end
-     NIRS.Dt.fir.pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]); 
-    end               
 end
-disp(['Folder adjustment location: ', dirrootnew]);
-if isfield(job.c_MultimodalPath,'b_MultimodalPath_yes')    
+%disp(['Folder adjustment location: ', dirrootnew]);
+if isfield(job.c_MultimodalPath,'b_MultimodalPath_yes')
     dirrootnew = job.c_MultimodalPath.b_MultimodalPath_yes.e_MultimodalPath{1};
-
-    if isfield(NIRS.Dt, 'AUX')     
+    
+    if isfield(NIRS.Dt, 'AUX')
         
-                for iAUX=1:numel(NIRS.Dt.AUX)      
-                    for imodule = 1:numel(NIRS.Dt.AUX(iAUX).pp)
-                        for ifile = 1:numel(NIRS.Dt.AUX(iAUX).pp(imodule).p)
-                                fileloc_previous = NIRS.Dt.AUX(iAUX).pp(imodule).p{ifile};
-                                [dir1,file1,ext1]=fileparts(fileloc_previous);
-                                NIRS.Dt.AUX(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
-                        end                    
-                    end 
+        for iAUX=1:numel(NIRS.Dt.AUX)
+            for imodule = 1:numel(NIRS.Dt.AUX(iAUX).pp)
+                for ifile = 1:numel(NIRS.Dt.AUX(iAUX).pp(imodule).p)
+                    fileloc_previous = NIRS.Dt.AUX(iAUX).pp(imodule).p{ifile};
+                    if ~isempty(fileloc_previous)
+                        [dir1,file1,ext1]=fileparts(fileloc_previous);
+                    if strcmpi(dir1(end-5:end),dirrootnew(end-5:end))
+                        NIRS.Dt.AUX(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
+                    else
+                        tmpdir=split(dir1,filesep);
+                        tmpdir1=fullfile(tmpdir{1:end-1});
+                        if strcmpi(tmpdir1(end-5:end),dirrootnew(end-5:end))
+                            NIRS.Dt.AUX(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew ,tmpdir{end}, [file1,ext1]);
+                        end
+                    end
+                    end
                 end
-    end
-    if isfield(NIRS.Dt, 'EEG')  
-        for iAUX=1:numel(NIRS.Dt.EEG)      
-                    for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
-                        for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
-                                fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
-                                [dir1,file1,ext1]=fileparts(fileloc_previous);
-                                NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
-                        end 
-                    end 
+            end
         end
     end
-    if isfield(NIRS.Dt, 'VIDEO')  
-%         for iAUX=1:numel(NIRS.Dt.EEG)      
-%                     for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
-%                         for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
-%                                 fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
-%                                 [dir1,file1,ext1]=fileparts(fileloc_previous);
-%                                 NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
-%                         end 
-%                     end 
-%         end
+    if isfield(NIRS.Dt, 'EEG')
+        for iAUX=1:numel(NIRS.Dt.EEG)
+            for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
+                for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
+                    fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
+                    [dir1,file1,ext1]=fileparts(fileloc_previous);
+                    NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
+                end
+            end
+        end
+    end
+    if isfield(NIRS.Dt, 'VIDEO')
+        %         for iAUX=1:numel(NIRS.Dt.EEG)
+        %                     for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
+        %                         for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
+        %                                 fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
+        %                                 [dir1,file1,ext1]=fileparts(fileloc_previous);
+        %                                 NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
+        %                         end
+        %                     end
+        %         end
     end
     
-        if isfield(NIRS.Dt, 'VIDEO')  
-%         for iAUX=1:numel(NIRS.Dt.EEG)      
-%                     for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
-%                         for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
-%                                 fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
-%                                 [dir1,file1,ext1]=fileparts(fileloc_previous);
-%                                 NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
-%                         end 
-%                     end 
-%         end
+    if isfield(NIRS.Dt, 'VIDEO')
+        %         for iAUX=1:numel(NIRS.Dt.EEG)
+        %                     for imodule = 1:numel(NIRS.Dt.EEG(iAUX).pp)
+        %                         for ifile = 1:numel(NIRS.Dt.EEG(iAUX).pp(imodule).p)
+        %                                 fileloc_previous = NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile};
+        %                                 [dir1,file1,ext1]=fileparts(fileloc_previous);
+        %                                 NIRS.Dt.EEG(iAUX).pp(imodule).p{ifile} = fullfile( dirrootnew , [file1,ext1]);
+        %                         end
+        %                     end
+        %         end
     end
 end
 
